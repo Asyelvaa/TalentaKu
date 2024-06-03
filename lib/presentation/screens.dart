7ee8399a22130d7a41e427b98313d7e7aa900eed
Asyelvaa <asyellaveratia24@gmail.com>
2024-06-03 08:59:02 +0700

    [LV] rename file grade to class_page

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,7 +1,7 @@
 export 'package:flutter_talentaku/presentation/AddEditPost/add_edit_post.screen.dart';
 export 'package:flutter_talentaku/presentation/class_detail/class_detail.screen.dart';
 export 'package:flutter_talentaku/presentation/daily_report/daily_report.screen.dart';
-export 'package:flutter_talentaku/presentation/grade_page/grade.screen.dart';
+export 'package:flutter_talentaku/presentation/grade_page/class_page.screen.dart';
 export 'package:flutter_talentaku/presentation/home_page/home_page.screen.dart';
 export 'package:flutter_talentaku/presentation/profile_page/profile_page.screen.dart';
 export 'package:flutter_talentaku/presentation/style_album/style_album.screen.dart';
